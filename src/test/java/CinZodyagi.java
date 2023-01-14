21b35d2d2e83ba67e96cfa742b79fcd7e1d78629
eborhan <106884905+eborhan@users.noreply.github.com>
2023-01-15 00:06:37 +0300

    Commit

--- src/test/java/CinZodyagi.java
+++ src/test/java/CinZodyagi.java
@@ -4,7 +4,7 @@ public class CinZodyagi {
     public static void main(String[] args) {
 
         int odd,birthYear;
-        String zodiac = "";
+        String zodiac = null;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Doğum yılınızı giriniz: ");
